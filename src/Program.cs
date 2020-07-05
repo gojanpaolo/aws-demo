81b02fa7945e4bad634663ffdda92be8252c1361
Jan Paolo Go <go.janpaolo@gmail.com>
2020-07-04 23:25:51 -0500

    cleanup src more

--- src/Program.cs
+++ src/Program.cs
@@ -1,26 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebApplication1
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static void Main(string[] args) => 
+            Host
+            .CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(_ => _
+                .Configure(_ => _
+                    .Run(_ => _
+                        .Response
+                        .WriteAsync("2"))))
+            .Build()
+            .Run();
     }
 }
