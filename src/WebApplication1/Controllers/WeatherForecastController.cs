e85f463d992b65933edf2100bce6270af6b6605f
Jan Paolo Go <go.janpaolo@gmail.com>
2020-07-04 22:31:31 -0500

    Update WeatherForecastController.cs

--- src/WebApplication1/Controllers/WeatherForecastController.cs
+++ src/WebApplication1/Controllers/WeatherForecastController.cs
@@ -24,16 +24,6 @@ namespace WebApplication1.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+        public string Get() => "1";
     }
 }
