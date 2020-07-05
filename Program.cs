0b4414f5ee664ce1b98da7168db2fa066e9e06d1
Jan Paolo Go <go.janpaolo@gmail.com>
2020-07-04 23:30:50 -0500

    clean repo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static void Main() => 
+        Host
+        .CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(_ => _
+            .Configure(_ => _
+                .Run(_ => _
+                    .Response
+                    .WriteAsync("3"))))
+        .Build()
+        .Run();
+}
